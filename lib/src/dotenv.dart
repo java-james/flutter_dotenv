bcf47e589b92218c4074f726c20b32ce1a3d1345
Xing Yu <ng.xingyu@dhs.sg>
2021-05-01 23:01:57 +0800

    Wrap dotenv method within class

--- lib/src/dotenv.dart
+++ lib/src/dotenv.dart
@@ -28,65 +28,63 @@ import 'parser.dart';
 ///     const _requiredEnvVars = const ['host', 'port'];
 ///     bool get hasEnv => isEveryDefined(_requiredEnvVars);
 ///
-bool _isInitialized = false;
-Map<String, String> _envMap = {};
 
-/// A copy of variables loaded at runtime from a file + any entries from mergeWith when loaded.
-Map<String, String> get env {
-  if (!_isInitialized) {
-    throw NotInitializedError();
-  }
-  return _envMap;
-}
+DotEnv dotenv = DotEnv();
 
-/// Clear [env]
-void clean() => _envMap.clear();
+class DotEnv {
+  bool _isInitialized = false;
+  final Map<String, String> _envMap = {};
 
-/// Loads environment variables from the env file into a map
-/// Merge with any entries defined in [mergeWith]
-Future load(
-    {String fileName = '.env',
-    Parser parser = const Parser(),
-    Map<String, String> mergeWith = const {}}) async {
-  clean();
-  final linesFromFile = await _getEntriesFromFile(fileName);
-  final linesFromMergeWith =
-      mergeWith.entries.map((entry) => "${entry.key}=${entry.value}").toList();
-  final allLines = linesFromMergeWith..addAll(linesFromFile);
-  final envEntries = parser.parse(allLines);
-  _envMap.addAll(envEntries);
-  _isInitialized = true;
-}
-
-Future testLoad(
-    {String fileInput = '',
-    Parser parser = const Parser(),
-    Map<String, String> mergeWith = const {}}) async {
-  clean();
-  final linesFromFile = fileInput.split('\n');
-  final linesFromMergeWith =
-      mergeWith.entries.map((entry) => "${entry.key}=${entry.value}").toList();
-  final allLines = linesFromMergeWith..addAll(linesFromFile);
-  final envEntries = parser.parse(allLines);
-  _envMap.addAll(envEntries);
-  _isInitialized = true;
-}
-
-/// True if all supplied variables have nonempty value; false otherwise.
-/// Differs from [containsKey](dart:core) by excluding null values.
-/// Note [load] should be called first.
-bool isEveryDefined(Iterable<String> vars) =>
-    vars.every((k) => _envMap[k]?.isNotEmpty ?? false);
-
-Future<List<String>> _getEntriesFromFile(String filename) async {
-  try {
-    WidgetsFlutterBinding.ensureInitialized();
-    var envString = await rootBundle.loadString(filename);
-    if (envString.isEmpty) {
-      throw EmptyEnvFileError();
+  /// A copy of variables loaded at runtime from a file + any entries from mergeWith when loaded.
+  Map<String, String> get env {
+    if (!_isInitialized) {
+      throw NotInitializedError();
     }
-    return envString.split('\n');
-  } on FlutterError {
-    throw FileNotFoundError();
+    return _envMap;
+  }
+
+  /// Clear [env]
+  void clean() => _envMap.clear();
+
+  /// Loads environment variables from the env file into a map
+  /// Merge with any entries defined in [mergeWith]
+  Future<void> load(
+      {String fileName = '.env', Parser parser = const Parser(), Map<String, String> mergeWith = const {}}) async {
+    clean();
+    final linesFromFile = await _getEntriesFromFile(fileName);
+    final linesFromMergeWith = mergeWith.entries.map((entry) => "${entry.key}=${entry.value}").toList();
+    final allLines = linesFromMergeWith..addAll(linesFromFile);
+    final envEntries = parser.parse(allLines);
+    _envMap.addAll(envEntries);
+    _isInitialized = true;
+  }
+
+  Future<void> testLoad(
+      {String fileInput = '', Parser parser = const Parser(), Map<String, String> mergeWith = const {}}) async {
+    clean();
+    final linesFromFile = fileInput.split('\n');
+    final linesFromMergeWith = mergeWith.entries.map((entry) => "${entry.key}=${entry.value}").toList();
+    final allLines = linesFromMergeWith..addAll(linesFromFile);
+    final envEntries = parser.parse(allLines);
+    _envMap.addAll(envEntries);
+    _isInitialized = true;
+  }
+
+  /// True if all supplied variables have nonempty value; false otherwise.
+  /// Differs from [containsKey](dart:core) by excluding null values.
+  /// Note [load] should be called first.
+  bool isEveryDefined(Iterable<String> vars) => vars.every((k) => _envMap[k]?.isNotEmpty ?? false);
+
+  Future<List<String>> _getEntriesFromFile(String filename) async {
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+      var envString = await rootBundle.loadString(filename);
+      if (envString.isEmpty) {
+        throw EmptyEnvFileError();
+      }
+      return envString.split('\n');
+    } on FlutterError {
+      throw FileNotFoundError();
+    }
   }
 }
